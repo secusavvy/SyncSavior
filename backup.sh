544a04f5d6e6662dc646cfea83965e5762fe9a0d
Zack <168565323+secusavvy@users.noreply.github.com>
2024-08-18 12:27:21 -0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # Check to make sure the user has entered exactly 2 arguments
-if [ $# -ne 2 ]; then
+if [ $# -ne 2 ]
+then
     echo "Usage: backup.sh <source_directory> <target_directory>"
-    echo "Please try again."
+    echo "Please try again:)"
     exit 1
 fi
 
 # Check if rsync is installed
-if ! command -v rsync > /dev/null 2>&1; then
+if ! command -v rsync > /dev/null 2>&1
+then
     echo "This script requires rsync to be installed."
     echo "Please use your distribution's package manager to install it and try again."
     exit 2
